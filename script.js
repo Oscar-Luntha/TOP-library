f87be7af986439b0b0d296fa495b8d898df7afe3
Oscar Luntha <lunthaoscar@gmail.com>
2025-12-13 22:20:46 +0200

    add a function for creating a book and adding it to the library collection

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-function Books(id, title , author, pages, readStatus){
+const myLibrary = [
+
+]
+function Book(id, title , author, pages, readStatus){
     this.id = id;
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.readStatus = readStatus;
+}
+function createBook(title , author, pages, readStatus){
+    const id = crypto.randomUUID()
+    const readStatus = false
+    const book = new Book(id, title, author, pages, readStatus)
+    myLibrary.push(book)
 }
