bbf7161e0b6e02b7f17062dec8ba8ec967001a25
Oscar Luntha <lunthaoscar@gmail.com>
2025-12-13 21:57:12 +0200

    Create a Books constructor

---
+++ script.js
@@ -0,0 +1,7 @@
+function Books(id, title , author, pages, readStatus){
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readStatus = readStatus;
+}
